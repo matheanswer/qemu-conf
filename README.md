68490600cad19b2bec2184ec9ecd0a463f956965
Mathias Heyraud <mathias.heyraud@gmail.com>
2022-11-20 21:52:00 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # qemu-conf
-QEMU/KVM and cloud-init host configuration
+QEMU/KVM with cloud-init and noVNC host configuration
+
+Pull cloud image from 
+
+```
+https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudimg.qcow2
+```
---
+++ arch.zsh
@@ -0,0 +1,37 @@
+#!/usr/bin/zsh
+number=${1:-"0"}
+host="10.0.0.3"
+printf -v macaddr "52:54:00:00:00:%02x" $number
+port=$((5700 + $number))
+url="http://vnc.home?path=&host=$host&port=$port"
+
+echo "noVNC : $url"
+
+cloud-localds images/seed.img user-data.yaml meta-data.yaml
+
+#create snapshot
+#qemu-img create -b Arch-Linux-x86_64-cloudimg.qcow2 -F qcow2 -f qcow2 images/Arch-Linux-x86_64-cloudimg-snapshot.qcow2 20G
+
+#copy and resize
+#cp images/Arch-Linux-x86_64-cloudimg.qcow2 images/Arch-Linux-x86_64-cloudimg-copy.qcow2
+#qemu-img resize images/Arch-Linux-x86_64-cloudimg-snapshot.qcow2 20G
+
+printf -v version %s $(qemu-system-x86_64 --version | head -1 | sed 's/QEMU emulator version //')
+
+OPTIONS=(
+-m 4G
+-smp 4
+-cpu host
+-enable-kvm
+-device virtio-tablet-pci
+-nic bridge,br=br0,model=virtio-net-pci,mac=$macaddr
+-drive if=virtio,format=qcow2,file=images/Arch-Linux-x86_64-cloudimg-snapshot.qcow2
+-drive if=virtio,format=raw,file=images/seed.img
+-smbios type=1,manufacturer="QEMU",product="QEMU/KVM",version="$version",family="Virtual Machine"
+-vnc :$number,websocket=on
+#set boot device, c=disk, d=cdrom
+-snapshot
+#-nographic
+)
+
+qemu-system-x86_64 "${OPTIONS[@]}"
